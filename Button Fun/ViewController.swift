d2c19aefc919fbb9c3fbf7e061262eb535cdbee6
Josh Elliott <josh@joshs-macbook-pro.local>
2018-09-19 16:16:00 -0400

    Initial Commit

---
+++ Button Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Button Fun
+//
+//  Created by Josh Elliott on 9/19/18.
+//  Copyright © 2018 JoshElliott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
